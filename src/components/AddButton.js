30e074f9bafeda37e4479b515f4b52eb50c204f4
Daiana K Szwimer <d.szwimer@distillery.com>
2020-12-12 14:12:31 -0300

    add list borders

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,7 +1,9 @@
 import AddIcon from '../assets/add.svg'
 
 const addButton = (props) => (
-    <button onClick={props.onClick} class="border border-gray-300 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-300 focus:outline-none focus:shadow-outline">
+    <button onClick={props.onClick} class="border border-gray-300 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-300 focus:outline-none focus:shadow-outline"
+        style={{width: '59px'}}
+    >
         <img src={AddIcon} alt='Add' style={{
             width: '25px'
         }}/>
